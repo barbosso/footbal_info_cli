b4479e20e4d05f8e430b32994746a880fb104cb5
barbosso <kalilinuxxx662@gmail.com>
2022-10-16 03:23:12 +0300

    change source info

--- main.py
+++ main.py
@@ -13,61 +13,76 @@ headers = {
 
 def get_table():
     table = PrettyTable()
-    table.field_names = ["P", "Commands", "Games", "Wins", "Drawn", "Loss", "Points"]
+    table.field_names = ["Поз", "Команда", "И", "В", "Н", "П", "З", "Пр", "+/-", "O"]
     table.align = "l"
     table.set_style(DOUBLE_BORDER)
-    r = requests.get("https://m.sports.ru/rfpl/table/", headers=headers)
+    r = requests.get("https://soccer365.ru/competitions/13/", headers=headers)
     soup = BeautifulSoup(r.text, "lxml")
-    commands = soup.findAll("tr", class_="b-tag-table__content-team")
+    commands = soup.find("div", id="competition_table").find("tbody").findAll("tr")
     for command in commands:
         item = command.findAll("td")
-        position = item[1].text
-        name = item[2].text
-        games = item[3].text
-        wins = item[4].text
-        drawn = item[5].text
-        loss = item[6].text
-        point = item[7].text
-        table.add_row([position, name, games, wins, drawn, loss, point])
+        position = item[0].text
+        club_name = item[1].find("a").text
+        games = item[2].text
+        wins = item[3].text
+        drawn = item[4].text
+        loss = item[5].text
+        goals = item[6].text
+        conceded_goals = item[7].text
+        difference = item[8].text
+        points = item[9].text.strip()
+        table.add_row([position, club_name, games, wins, drawn, loss, goals, conceded_goals, difference, points])
     print(table)
 
 
-def get_calendar():
-    r = requests.get("https://m.sports.ru/rfpl/calendar/", headers=headers)
-    soup = BeautifulSoup(r.text, "lxml")
-    one_tur = soup.findAll("section", class_="b-tag-tournament-calendar__stage")
-    del one_tur[0:2]
-    del one_tur[1:]
+def get_current_tur():
     table = PrettyTable()
-    table.field_names = ["Дом", "Счет", "Гости", "Дата", "Статус"]
+    table.field_names = ["Время", "Дом", "Счет", "Гости"]
     table.align = "l"
     table.set_style(DOUBLE_BORDER)
-    for tur in one_tur:
-        head = tur.find("h5", class_="g-section-header").text
-        matches = tur.findAll("article", class_="b-tag-tournament-calendar__stage-match")
-        table.title = head
-        for match in matches:
-            team1 = match.findAll("div", class_="g-match-block-a__team-name")[0].text
-            team2 = match.findAll("div", class_="g-match-block-a__team-name")[1].text
-            gamescore = match.find("div", class_="g-score-a").text
-            link_match = match.find("a").get("href")
-            match_status = status_match(link_match).strip()
-            matchday = match.find("div", class_="g-match-block-a__info").text[:-6]
-            matchtime = match.find("div", class_="g-match-block-a__info").text[-6:]
-            table.add_row([team1, gamescore, team2, matchday + " " + matchtime, match_status])
-
-        print(table)
-
-
-def status_match(url):
-    """
-    the function takes a link to the match and returns whether the match is completed or not
-    """
-    r = requests.get(url, headers=headers)
+    r = requests.get("https://soccer365.ru/competitions/13/", headers=headers)
     soup = BeautifulSoup(r.text, "lxml")
-    status = soup.find("div", class_="match-summary__state-info").find("span",
-                                                                       class_="match-summary__state-status").text
-    return status
+    calendar_tour = soup.find("div", id="next_tur")
+    tur = calendar_tour.find("div", class_="block_header").text
+    matches = calendar_tour.findAll("div", class_="game_block")
+    table.title = tur
+    for match in matches:
+        status_match = match.find("div", class_="status").text
+        team1 = match.findAll("div", class_="name")[0].text
+        team2 = match.findAll("div", class_="name")[1].text
+        team1_goals = match.findAll("div", class_="gls")[0].text
+        team2_goals = match.findAll("div", class_="gls")[1].text
+        table.add_row([status_match, team1, team1_goals + " : " + team2_goals, team2])
+    print(table)
+
+
+
+def get_next_tur():
+    table = PrettyTable()
+    table.field_names = ["Время", "Дом", "Счет", "Гости"]
+    table.align = "l"
+    table.set_style(DOUBLE_BORDER)
+    r = requests.get("https://soccer365.ru/competitions/13/shedule/", headers=headers)
+    soup = BeautifulSoup(r.text, "lxml")
+    calendar_tour = soup.find("div", class_="live_comptt_bd").findNext("div", class_="live_comptt_bd")
+    tur = calendar_tour.find("div", class_="cmp_stg_ttl").text
+    matches = calendar_tour.findAll("div", class_="game_block")
+    table.title = tur
+    for match in matches:
+        team1 = match.findAll("div", class_="name")[0].text
+        team2 = match.findAll("div", class_="name")[1].text
+        status_match = match.find("div", class_="status").find("span").text
+        team1_goals = match.findAll("div", class_="gls")[0].text
+        team2_goals = match.findAll("div", class_="gls")[1].text
+        table.add_row([status_match, team1, team1_goals + " : " + team2_goals, team2])
+    print(table)
+
+#
+# def get_table_bombardirs():
+#     r = requests.get("https://soccer365.ru/competitions/13/", headers=headers)
+#     soup = BeautifulSoup(r.text, "lxml")
+#     tableb = soup.find("div", class_="live_comptt_bd")
+
 
 
 def main():
@@ -76,16 +91,17 @@ def main():
             'type': 'list',
             'name': 'user_option',
             'message': 'Welcome to RFPL',
-            'choices': ["Таблица", "Расписание"]
+            'choices': ["Таблица", "Текущий Тур", "Следующий Тур"]
         }
     ]
 
     answers = prompt(questions, style=custom_style_2)
     if answers.get("user_option") == "Таблица":
         get_table()
-    elif answers.get("user_option") == "Расписание":
-        get_calendar()
-
+    elif answers.get("user_option") == "Текущий Тур":
+        get_current_tur()
+    elif answers.get("user_option") == "Следующий Тур":
+        get_next_tur()
 
 if __name__ == "__main__":
     main()
